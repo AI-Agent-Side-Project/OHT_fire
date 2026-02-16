c7b3f19f02981c362678f730db257603ec8e818d
sunghoonjung <shoooon.j@gmail.com>
2026-02-16 11:25:03 +0900

    Dataset Preparation

--- run.py
+++ run.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
     # basic config
     parser.add_argument('--multimodal', action='store_true', help='use multimodal data', default=False)
     parser.add_argument('--task_name', type = str, required = True, default = 'classification')
-    parser.add_argument('--is_training', type = int, required = True, default = 1, help = 'status')
+    parser.add_argument('--is_training', type = int, required = True, default = 0, help = 'status')
     parser.add_argument('--model', type = str, required = True, default = 'TimesNet')
 
     # data loader
@@ -38,25 +38,25 @@ if __name__ == '__main__':
     parser.add_argument('--train_epochs', type=int, default=10, help='train epochs')
     parser.add_argument('--num_workers', type=int, default=10, help='data loader num workers')
     parser.add_argument('--itr', type=int, default=1, help='experiments times')
-    parser.add_argument('--batch_size', type=int, default=32, help='batch size of train input data')
+    parser.add_argument('--batch_size', type=int, default=24, help='batch size of train input data')
     parser.add_argument('--patience', type=int, default=3, help='early stopping patience')
     parser.add_argument('--learning_rate', type=float, default=0.0001, help='optimizer learning rate')
 
     # model define
     parser.add_argument('--enc_in', type=int, default=7, help='encoder input size')
     parser.add_argument('--dec_in', type=int, default=7, help='decoder input size')
-    parser.add_argument('--top_k', type=int, default=5, help='for TimesBlock')
+    parser.add_argument('--top_k', type=int, default=3, help='for TimesBlock')
     parser.add_argument('--num_kernels', type=int, default=6, help='for Inception')
-    parser.add_argument('--d_model', type=int, default=512, help='dimension of model')
-    parser.add_argument('--d_ff', type=int, default=2048, help='dimension of fcn')
+    parser.add_argument('--d_model', type=int, default=32, help='dimension of model')
+    parser.add_argument('--d_ff', type=int, default=64, help='dimension of fcn')
     parser.add_argument('--e_layers', type=int, default=2, help='num of encoder layers')
     parser.add_argument('--dropout', type=float, default=0.1, help='dropout')
     parser.add_argument('--embed', type=str, default='timeF',
                         help='time features encoding, options:[timeF, fixed, learned]')
 
     # data loader
-    parser.add_argument('--stride' , type=int, default=1, help='data stride')
-    parser.add_argument('--seq_len', type=int, default=96, help='input sequence length')
+    parser.add_argument('--stride' , type=int, default=4, help='data stride')
+    parser.add_argument('--seq_len', type=int, default=32, help='input sequence length')
     parser.add_argument('--data', type=str, required=True, default='OHT_fire', help='dataset type')
     parser.add_argument('--root_path', type=str, default='./database/OHT/', help='root path of the data file')
     parser.add_argument('--freq', type=str, default='h',
